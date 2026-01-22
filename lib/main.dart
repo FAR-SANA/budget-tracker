892a680ef35a3694579b297db438580313e363b4
FatimaSanaIssahac <fatimasanaissahac543@gmail.com>
2026-01-22 15:12:42 +0530

    merging w farsana

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'screens/login_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // ✅ Initialize Supabase
   await Supabase.initialize(
     url: 'https://zpfqupnigkvfrjukuquq.supabase.co',
     anonKey: 'sb_publishable_iTpvGf7x_nu48jJYcc88oA__SWIRoB-',
   );
 
-  runApp(const MyApp());
+  runApp(const BudgeeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BudgeeApp extends StatelessWidget {
+  const BudgeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Supabase Connected ✅'))),
+      title: 'Budgee',
+
+      // ✅ Start with Login Screen
+      home: const LoginScreen(),
     );
   }
 }
